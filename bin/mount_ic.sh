d71fe61f3eb9f3c226ed2b12a1918ee74272ad37
Sam Rogerson <samr@cern.ch>
2012-03-02 13:21:12 +0100

    more automation

--- bin/mount_ic.sh
+++ bin/mount_ic.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
 
-sshfs sr505@lx06.hep.ph.ic.ac.uk:/vols /mnt/vols
-sshfs sr505@lx06.hep.ph.ic.ac.uk:public_html /mnt/public_html
+local_folders[0]="/mnt/vols"
+local_folders[1]="/mnt/public_html"
+remote_folders[0]="/vols"
+remote_folders[1]="public_html"
+
+for((i=0;i<${#local_folders[@]};i+=1)); do
+    if [[ ! -d $local_folders[$i] ]]; then
+        echo "${local_folders[$i]} doesn't exist exists"
+        read -n1 -p "Create ${local_folders[$i]} (y/n)?"
+        echo
+        if [[ "$REPLY" == [yY] ]]; then
+            echo Making ${local_folders[$i]}
+            sudo mkdir ${local_folders[$i]}
+            sudo chown root:users ${local_folders[$i]}
+        else
+            echo Skipping ${local_folders[$i]}
+            continue
+        fi
+    fi
+    sshfs sr505@lx06.hep.ph.ic.ac.uk:${remote_folders[$i]} ${local_folders[$i]}
+done

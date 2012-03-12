66d8998b20768ced76e72ea66f6bf5176cb0156e
Sam Rogerson <samr@cern.ch>
2012-03-12 16:19:09 +0100

    function call

--- bin/dzen/maildir.sh
+++ bin/dzen/maildir.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
-local -A counts; local i
+fmail()  {
+    local -A counts; local i
 
-for i in "${MAILDIR:-${HOME}/Mail}"/**/new/*
-    { (( counts[${i:h:h:t}]++ )) }
-for i in ${(k)counts}
-    { print -n $i: $counts[$i]' ' }
+    for i in "${MAILDIR:-${HOME}/Mail}"/**/new/*
+        { (( counts[${i:h:h:t}]++ )) }
+    for i in ${(k)counts}
+        { print -n $i: $counts[$i]' ' }
+}
+
+fmail

452d63b624c148af6730b47cea117749df2bf367
Sam Rogerson <samr@cern.ch>
2012-03-12 16:18:34 +0100

    update

---
+++ bin/dzen/maildir.sh
@@ -0,0 +1,8 @@
+fmail() {
+    local -A counts; local i
+ 
+    for i in "${MAILDIR:-${HOME}/Mail}"/**/new/*
+        { (( counts[${i:h:h:t}]++ )) }
+    for i in ${(k)counts}
+        { print -n $i: $counts[$i]' ' }
+}

d535789bc2dba30ed4e814f39547b9d2c54b78b0
Sam Rogerson <samr@cern.ch>
2012-03-12 21:28:41 +0100

    various fixes: file should be discarded

--- bin/dzen/bar.sh
+++ bin/dzen/bar.sh
@@ -1,23 +1,12 @@
-#!/bin/zsh
-#
-# Usage: ./bar.sh [<desktop_number>]
-#
-# Creates a clickable dzen bar that allows switching between desktops,
-# optionally switching to the given desktop.
-
+#! /bin/bash
 FG_FOCUS="#F0DFAF"
-BG_FOCUS="#1E2320"
-
-#Launch dzen2 and establish the pipe
-if [[ ! -e /tmp/dzen2.pipe ]]
-then
-  socat -v PIPE:/tmp/dzen2.pipe EXEC:"dzen2 -p -ta l -x 0 -w 200 -fn -xos4-terminus-medium-r-normal--12-140-72-72-c-80-iso8859-1"&
-fi
+#BG_FOCUS="#1E2320"
+BG_FOCUS="#002b36"
 
 #Switch to the given desktop
-if [[ ! -z $1 ]]
+if [[ ! -z $2 ]]
 then
-  wmctrl -s $1
+  wmctrl -s $2
 fi
 
 #Get all desktop names
@@ -29,22 +18,16 @@ DESKTOPS=(`wmctrl -d | cut -d\  -f13`)
 #
 #The wm mappings to switch between desktop should call this script as well
 line=""
-for d in $DESKTOPS
+for d in ${DESKTOPS[@]}
 do
-  switchTo=`wmctrl -d | grep $d | cut -d\  -f1`
-  if [[ -z "`wmctrl -d | grep $d | grep '*'`" ]]
-  then
-    line+="^ca(1,./bar.sh $switchTo)$d^ca()"
-  else
-    line+="^ca(1,./bar.sh $switchTo)^bg($BG_FOCUS)^fg($FG_FOCUS)$d^fg()^bg()^ca()"
-  fi
+    switchTo=`wmctrl -d | grep $d | cut -d\  -f1`
+    is_curr=`wmctrl -d | grep $d | grep '*'`
+    if [[ $is_curr == ""  ]]
+    then
+        line+="^ca(1,./bar.sh $1 $switchTo >> $1) $d ^ca()"
+    else
+        line+="^ca(1,./bar.sh $1 $switchTo >> $1)^bg($BG_FOCUS)^fg($FG_FOCUS) $d ^fg()^bg()^ca()"
+    fi
 done
 
-#Forward the dzen input to dzen through the pipe
-while true; do
-    echo $line 
-    sleep 1
-done | dzen2 -ta l -x 0 -w 200 -fn -xos4-terminus-medium-r-normal--12-140-72-72-c-80-iso8859-1
-#socat - PIPE:/tmp/dzen2.pipe
-
-sleep 10
+echo $line

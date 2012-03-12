c0852dcfca0d644c0da81ecddf7ec8c54a88b90e
Sam Rogerson <samr@cern.ch>
2012-03-12 16:20:36 +0100

    specify custom width on each instance

--- bin/dzen/dzen.sh
+++ bin/dzen/dzen.sh
@@ -20,17 +20,17 @@ Y=0
 FONT="-xos4-terminus-medium-r-normal--12-140-72-72-c-80-iso8859-1"
 ACT='button1=togglecollapse;button3=exit'
 
-DZEN_OPTS="-fn $FONT -fg $FG -bg $BG -y $Y -sa l -ta l -w $WIDTH"
+DZEN_OPTS="-fn $FONT -fg $FG -bg $BG -y $Y -sa l -ta l"
 
 # gmail -l 5
 # mem -l 3
 
 dz_kill
-./mem.sh | dzen2 $DZEN_OPTS -x 0 -l 3  &
-./cpu.sh | dzen2 $DZEN_OPTS -x 150 &
-./gmail.sh | dzen2 $DZEN_OPTS -x 300 -l 5 &
-./network.sh | dzen2 $DZEN_OPTS -x 450 &
-./date.sh | dzen2 $DZEN_OPTS -x 800 &
+./mem.sh | dzen2 $DZEN_OPTS -x 0 -w 150 -l 3 &
+./cpu.sh | dzen2 $DZEN_OPTS -x 150 -w 150 &
+./gmail.sh | dzen2 $DZEN_OPTS -x 300 -w 150 -l 5 &
+./network.sh | dzen2 $DZEN_OPTS -x 450 -w 150 &
+./date.sh | dzen2 $DZEN_OPTS -x 800 -w 150 &
 
 sleep 10
 dz_kill

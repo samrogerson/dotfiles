4374ae414fcb2b44cbfdaaea099d5a1d27c09200
Sam Rogerson <samr@cern.ch>
2012-03-12 13:31:28 +0100

    skeleton for base dzen options

---
+++ bin/dzen/dzen.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+FG="grey70"
+BG="#002b36"
+FONT="-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-1"
+TITLE_WIDTH=125
+YPOS=0
+dzen2 -ta l -l 3 -fn $FONT -bg $BG -fg $BG  -tw $TITLE_WIDTH -y $YPOS -sa c

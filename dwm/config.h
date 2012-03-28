5b224d8f0454aa943bd6520881e294695ad259b5
Sam Rogerson <samr@cern.ch>
2012-03-28 19:29:22 +0200

    sxiv float

--- dwm/config.h
+++ dwm/config.h
@@ -34,6 +34,7 @@ static const Rule rules[] = {
     { java_name,  NULL,       NULL,       1 << 5,       True,        -1 },
 	{ NULL,       NULL,       "Vievo",    1 << 5,       True,        -1 },
 	{ "Skype",    NULL,       NULL,       1 << 7,       False,       -1 },
+	{ "sxiv",     NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */

f8b197f3de847bbb2d8a17b19fe4407963bf1e75
Sam Rogerson <samr@cern.ch>
2012-06-21 13:12:31 +0200

    float alsamixer

--- dwm/config.h
+++ dwm/config.h
@@ -19,6 +19,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const char java_name[] = "net-sourceforge-jnlp-runtime-Boot";
 static const char nmon_name[] = "wicd-curses";
+static const char vcon_name[] = "alsamixer";
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -40,6 +41,7 @@ static const Rule rules[] = {
 	{ "Canvas",   NULL,       NULL,       0,            True,        -1 },
 	{ "Wine",     NULL,       NULL,       1 << 6,       True,        -1 },
 	{ NULL,       NULL,       nmon_name,  0,            True,        -1 },
+	{ NULL,       NULL,       vcon_name,  0,            True,        -1 },
 };
 
 /* layout(s) */

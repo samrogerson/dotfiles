72194458ac3c1bda8e48229bd6a0a19064678f45
Sam Rogerson <samr@cern.ch>
2012-07-03 13:34:17 +0100

    use conky as status

--- bin/dwmstatus.sh
+++ bin/dwmstatus.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-conky -x 0 -y 0 &
-
-#while true ; do
-    #date_str="$( date +"%F %R" )"
-    #battery_str="$(acpi -b | awk 'gsub(/,/,"") {print $3, $4}')"
-    #xsetroot -name  "$battery_str $date_str "
-    #sleep 1m
-#done
+conky -x 0 -y 0 | while read -r; do xsetroot -name "$REPLY"; done

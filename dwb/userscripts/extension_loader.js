28c42b2207a49784d76bdcdf7fd1c4438a232639
Sam Rogerson <samr@cern.ch>
2013-04-30 20:53:27 +0100

    add dwb settings

---
+++ dwb/userscripts/extension_loader.js
@@ -0,0 +1,22 @@
+//!javascript
+//<adblock_subscriptions___SCRIPT
+extensions.load("adblock_subscriptions", {
+//<adblock_subscriptions___CONFIG
+// To take effect dwb needs to be restarted
+
+// Shortcut to subscribe to a filterlist
+scSubscribe : null, 
+// Command to subscribe to a filterlist
+cmdSubscribe : "adblock_subscribe", 
+
+// Shortcut to unsubscribe from a filterlist
+scUnsubscribe : null, 
+
+// Command to unsubscribe from a filterlist
+cmdUnsubscribe : "adblock_unsubscribe",
+
+// Path to the filterlist directory, will be created if it doesn't exist. 
+filterListDir : data.configDir + "/adblock_lists"
+//>adblock_subscriptions___CONFIG
+});
+//>adblock_subscriptions___SCRIPT
